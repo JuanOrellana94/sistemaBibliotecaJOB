d4801fe6b3781c02bb68e5d046cea5ee93c293c8
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-08-01 12:27:46 -0600

    Update  Acciones.php
    faltaba data en este archivo, condiciones para navegacion en el area de operaciones

--- acciones.php
+++ acciones.php
@@ -14,8 +14,17 @@
 
  if ($pageLocation=="busqueda") {
     include("pages/operaciones/buscarMenu.php");
- } else if ($pageLocation=="none") {
+ } else if ($pageLocation=="prestamos") {
  	 include("top.php");
+ 	 include("pages/operaciones/opPrestamos.php");
+ 	# code...
+ }  else if ($pageLocation=="devoluciones") {
+ 	 include("top.php");
+ 	 include("pages/operaciones/opDevoluciones.php");
+ 	# code...
+ }  else if ($pageLocation=="historial") {
+ 	 include("top.php");
+ 	 include("pages/operaciones/opDevoluciones.php");
  	# code...
  }
 
