957739ec62b9c7c7894d9b2ffd89b885907b7cfa
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-08-01 21:26:37 -0600

    Actualizar top empotrado utilsrespaldo.php
    archivo utilrespaldo utiliza su propio codigo para top.php
    
    actualizado direccionamiento de prestamos y devoluciones en dicho codigo.

--- utilrespaldo.php
+++ utilrespaldo.php
@@ -118,9 +118,10 @@ TODO: zip del archivo backup, subir el zip a un google drive administrado por el
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           
-          <a class="dropdown-item" href="#">Prestar</a>
-          <a class="dropdown-item" href="#">Devoluciones</a>
-          <a class="dropdown-item" href="#">Historial</a>
+             
+        <a class="dropdown-item" href="acciones.php?pageLocation=historial">Historial</a>  
+        <a class="dropdown-item" href="acciones.php?pageLocation=prestamos">Prestar</a>
+        <a class="dropdown-item" href="acciones.php?pageLocation=devoluciones">Devoluciones</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item disabled" href="#">Operaciones</a>
         </div>
