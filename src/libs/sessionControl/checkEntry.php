35b0dbbfe462b23ecb7d6eae4946370e40c3ac23
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-08-19 14:08:42 -0600

    Fix checkEntry part II

--- src/libs/sessionControl/checkEntry.php
+++ src/libs/sessionControl/checkEntry.php
@@ -17,8 +17,10 @@ $UsuPassword=md5($_POST['usuContrasena']);
 
 
 
-
-$checkValidation="SELECT * from $tablaUsuarios WHERE $varCarnet='$usuAccount' AND $varContrasena='$UsuPassword'";
+$checkValidation="SELECT * from $tablaUsuarios WHERE 
+($varContrasena='$UsuPassword' AND $varCarnet='$usuAccount') OR 
+($varContrasena='$UsuPassword' AND $varAccNombre='$usuAccount');
+";
 
 $resultado=mysqli_query($conexion, $checkValidation) or die(mysqli_error($conexion));
 
