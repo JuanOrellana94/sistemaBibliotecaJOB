1aa7445114a589327976da47fe38ba6797014ad7
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-08-22 19:44:08 -0600

    Update down.php

--- down.php
+++ down.php
@@ -1,25 +1,31 @@
 
 <style>
-	 .textBottom {          
+   .textBottom {          
           color: white;
           font-size: 20px;
 
         }
+  .navbot{
+    position:absolute;
+    bottom:0;
+    right:0;
+    width: 100%
+  }
 </style>
+<br>
+<nav class="navbar navbot" style="background-color:#003764;">
+  <a class="navbar-brand " ></a>
 
-<nav class="navbar fixed-bottom " style="background-color:#003764;">
-  <a class="navbar-brand" ></a>
-
-	<div class="textBottom"><small>SISTEMA DE BIBLIOTECA, VERSION PROTOTIPO 1.0, 2019</small>
-  		<img src="img/icons/logoSys.png" width="55" height="50">  
-	</div>
+  <div class="textBottom"><small>SISTEMA DE BIBLIOTECA, VERSION PROTOTIPO 1.0, 2019</small>
+      <img src="img/icons/LogoSys.png" width="55" height="50">  
+  </div>
 </nav>
 
 </body>
 
 
 <script>
-	$(function () {
+  $(function () {
       $('[data-toggle="tooltip"]').tooltip()
     })
 </script> 
