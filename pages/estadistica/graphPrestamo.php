46009613849075f03044744970b5c60b0441511a
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-08-23 21:18:49 -0600

    UPDATE 433b
    >Fixes toward data stats on Graphs
    >Added [General data] info tab
    >Cleaned some code

--- pages/estadistica/graphPrestamo.php
+++ pages/estadistica/graphPrestamo.php
@@ -13,15 +13,14 @@ include("../../src/libs/sessionControl/conection.php");
 if (isset($_GET["tipo"])) { 
 		$typeSelect  = $_GET["tipo"]; 
 	} else {
-		$typeSelect='2	';
+		$typeSelect='3';
 	};
 
 
 if ($typeSelect==1) {
 	$yearSelect=date("Y");
 	
-	 $sqlQuery="SELECT DATE_FORMAT(resumen.$varprestfec,'%c') as Month, COUNT(detalle.$vardetcodlib) as counter FROM $tablaLibros as libro INNER JOIN $tablaEjemplares as ejemplares on libro.$varlibcod=ejemplares.$varejemlibcod INNER JOIN $vardetallesprestamolibro as detalle on ejemplares.$varejemcod=detalle.$varejemcodlib INNER JOIN $varresumenlibroprestamo as resumen on resumen.$varprestcod=detalle.$varprestcodlib 
-	 	 WHERE DATE_FORMAT(resumen.$varprestfec,'%Y') LIKE '%$yearSelect%'  GROUP BY DATE_FORMAT(resumen.$varprestfec,'%c');";
+	 $sqlQuery="SELECT DATE_FORMAT(resumen.$varprestfec,'%c') as Month, COUNT(detalle.$vardetcodlib) as counter FROM $tablaLibros as libro INNER JOIN $tablaEjemplares as ejemplares on libro.$varlibcod=ejemplares.$varejemlibcod INNER JOIN $vardetallesprestamolibro as detalle on ejemplares.$varejemcod=detalle.$varejemcodlib INNER JOIN $varresumenlibroprestamo as resumen on resumen.$varprestcod=detalle.$varprestcodlib WHERE DATE_FORMAT(resumen.$varprestfec,'%Y') LIKE '%$yearSelect%'  GROUP BY DATE_FORMAT(resumen.$varprestfec,'%c');";
 	//echo json_encode($sqlQuery);
 	$result = mysqli_query($conexion,$sqlQuery);
 
@@ -32,7 +31,7 @@ if ($typeSelect==1) {
 } else if ($typeSelect==2) {
 	$monthSelect=date("c");
 	$yearSelect=date("Y");
-	$sqlQuery="SELECT DATE_FORMAT(resumen.$varprestfec,'%c') as Month, COUNT(detalle.$vardetcodlib) as counter  FROM $vardetallesprestamolibro as detalle INNER JOIN $varresumenlibroprestamo as resumen on detalle.$varprestcodlib= resumen.$varprestcod INNER JOIN $tablaEjemplares as ejemplar on ejemplar.$varejemcod=detalle.$varejemcodlib  WHERE DATE_FORMAT(resumen.$varprestfechafin,'%Y')='$yearSelect' AND ejemplar.$varejemestu='0'GROUP BY DATE_FORMAT(resumen.$varprestfec,'%c');";                    
+	$sqlQuery="SELECT DATE_FORMAT(resumen.$varprestfec,'%c') as Month, COUNT(detalle.$vardetcodlib) as counter  FROM $vardetallesprestamolibro as detalle INNER JOIN $varresumenlibroprestamo as resumen on detalle.$varprestcodlib= resumen.$varprestcod INNER JOIN $tablaEjemplares as ejemplar on ejemplar.$varejemcod=detalle.$varejemcodlib  WHERE DATE_FORMAT(resumen.$varprestfechafin,'%Y')='$yearSelect' AND ejemplar.$varejemestu='0'GROUP BY DATE_FORMAT(resumen.$varprestfechafin,'%c');";                    
 	//echo json_encode($sqlQuery);
 	$result = mysqli_query($conexion,$sqlQuery);
 
@@ -55,7 +54,7 @@ if ($typeSelect==1) {
 
 } else if ($typeSelect==4) {
 	$yearSelect=date("Y");
-	$sqlQuery="SELECT DATE_FORMAT(resumen.$varprestfecequi,'%c') as Month, COUNT(detalle.$vardetcodequi) as counter  FROM $vardetallesprestamoequipo as detalle INNER JOIN $varresumenequipoprestamo as resumen on detalle.$varprestcodequi= resumen.$varprestcodequi INNER JOIN $tablaExistenciaequipo as existencia on existencia.$varexistcod=detalle.$varexistcodDet  WHERE DATE_FORMAT(resumen.$varprestcodequi,'%Y')='$yearSelect' AND existencia.$varexistestu='0'GROUP BY DATE_FORMAT(resumen.$varprestfec,'%c');";
+	$sqlQuery="SELECT DATE_FORMAT(resumen.$varprestfecequi,'%c') as Month, COUNT(detalle.$vardetcodequi) as counter  FROM $vardetallesprestamoequipo as detalle INNER JOIN $varresumenequipoprestamo as resumen on detalle.$varprestcodequi= resumen.$varprestcodequi INNER JOIN $tablaExistenciaequipo as existencia on existencia.$varexistcod=detalle.$varexistcodDet WHERE DATE_FORMAT(resumen.$varprestfechafinEquipo,'%Y')='$yearSelect' AND existencia.$varexistestu='0' GROUP BY DATE_FORMAT(resumen.$varprestfechafinEquipo,'%c');";
 	//echo json_encode($sqlQuery);
 	$result = mysqli_query($conexion,$sqlQuery);
 
--- pages/estadistica/verIndicadores.php
+++ pages/estadistica/verIndicadores.php
@@ -1,18 +1,18 @@
 
 <!--INICIO CONTENEDOR DE CATALOGO DE Estantes-->    
 <div class="card">
-  <div class="card-body">
-          <div class="row">        
-            <div class="col-lg-9 col-md-6">
-             <ul class="nav nav-tabs mb-3" id="pills-tab" role="tablist">
-                <li class="nav-item">
-                  <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#general" role="tab" aria-controls="pills-home" aria-selected="true">Datos Generales</a>
+  <div class="card-body" style="background-color: #d9d9d9;">
+    <div class="row">        
+      <div class="col-lg-9 col-md-6">
+        <ul class="nav nav-tabs mb-6" id="pills-tab" role="tablist">
+          <li class="nav-item">
+                  <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#general" role="tab" aria-controls="pills-home" aria-selected="true"><h5>Datos Generales</h5></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#reportes" role="tab" aria-controls="pills-profile" aria-selected="false">Reportes</a>
+                  <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#reportes" role="tab" aria-controls="pills-profile" aria-selected="false"><h5>Reportes</h5></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#solvencias" role="tab" aria-controls="pills-contact" aria-selected="false">Solvencias</a>
+                  <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#solvencias" role="tab" aria-controls="pills-contact" aria-selected="false"><h5>Solvencias</h5></a>
                 </li>
             </ul>                  
             </div>                
@@ -21,22 +21,40 @@
             
             </div>
           </div>
-
-
         <!--Cuerpo del panel--> 
         <div class="card-body">              
           <div class="row">            
             <div class="col-md-12">               
-
-                <div class="tab-content" id="estadisticaContent">
+              <div class="tab-content" id="estadisticaContent">
                   <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
                    
-                    <div id="repGeneral"></div>
-                    
+                    <div id="repGeneral"></div>                    
                   </div>
                   <div class="tab-pane fade" id="reportes" role="tabpanel" aria-labelledby="reportes-tab">
-                        <p>reportes</p>
-                       <div id="repReporte"></div>
+                        
+                       <div id="repReporte">
+
+                        <div class="row">
+                          <div class="col-lg-12">
+                            <div class="card">
+                              <div class="card-heder">
+                                <h5>Reportes</h5>
+                              </div>
+                              <div class="card-body">
+
+                                  
+                              </div>
+                              <div class="card-footer">
+                                <div class="text-muted">
+                                  Generar Codigos
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>                         
+
+
+                       </div>
                   </div>
                   <div class="tab-pane fade" id="solvencias" role="tabpanel" aria-labelledby="solvencias-tab">
                      <p>Retrasos</p>
@@ -97,6 +115,10 @@
 
 function dataDestacados(){
     $("#dataDestacados").load("pages/estadistica/detalleDestacados.php");
+  }
+
+  function datainfoAll(){
+    $("#infoAll").load("pages/estadistica/detalleBDS.php");
   }
 
 
