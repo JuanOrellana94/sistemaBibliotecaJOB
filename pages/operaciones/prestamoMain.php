b7c16c6b078cc6cb34de7fd7c8e9c3cd1952951a
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-09-05 11:54:28 -0600

    Update prestamoMain.php
    VOLUMNEN INPUT IS NOW DISABLED UNTIL A USER HAS BEEN ADDED

--- pages/operaciones/prestamoMain.php
+++ pages/operaciones/prestamoMain.php
@@ -44,7 +44,7 @@
 				<div class="col-sm-8" data-toggle="tooltip" data-placement="right"  title="Ingresa el codigo del ejemplar de libro que deseas prestar">
 					<div class="form-group" >
 					 	<h6 class="card-subtitle mb-2 text-muted" >Codigo del Ejemplar:</h6>
-      	                <input type="text" class="form-control" onfocus="this.value=''" name="textEjemplar" id="textEjemplar" aria-describedby="textEjemplar" placeholder="">
+      	                <input disabled type="text" class="form-control" onfocus="this.value=''" name="textEjemplar" id="textEjemplar" aria-describedby="textEjemplar" placeholder="">
       	                <div id="infoListaLibros"></div>
       	                <div id="solicitudDetallesDiv"></div>
 					</div>
