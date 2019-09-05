ec8fafec3f8c4f5a9633eb58455f01d640756ca5
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-09-05 12:23:58 -0600

    Update inicio.php
    Added green/red colors

--- inicio.php
+++ inicio.php
@@ -84,10 +84,10 @@
 
                 <div class="row">
                   <div class="col-sm-6">
-                        <button type="button" class="btn btnAccess" onclick="checkUser()" name="entryCheck">Ingresar</button>
+                        <button type="button" class="btn btnAccess" style="background-color:#5cd65c" onclick="checkUser()" name="entryCheck">Ingresar</button>
                   </div>
                   <div class="col-sm-6">
-                          <button type="button" class="btn btnAccess" onclick="goBack()" name="goBack">Volver</button>
+                          <button type="button" class="btn btnAccess" onclick="goBack()" style="background-color:#ff3333"  name="goBack">Volver</button>
                   </div>
                 </div>
 
