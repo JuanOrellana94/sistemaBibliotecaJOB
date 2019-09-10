58646cd866fea3d5040b3dd2b2e93d94e52dc0d7
JuanOrellana94 <52117049+JuanOrellana94@users.noreply.github.com>
2019-09-10 15:46:13 -0600

    Update creditos.php
    Add Veronica Herrera to credits list

--- creditos.php
+++ creditos.php
@@ -91,7 +91,8 @@
         <p class="card-text text-right">Lic Ernesto Anibal</p>
         <p class="card-text text-right">Hno. César Gonzáles</p>
        
-        <p class="card-text text-right">Lic Hilda Gomez</p> 
+        <p class="card-text text-right">Lic Hilda Gomez</p>
+          <p class="card-text text-right">Lic Verónica Herrera</p> 
       </div>
       <div class="col-lg-6">
          <p class="card-text text-left">Vilma Yaneth Alfaro</p>
